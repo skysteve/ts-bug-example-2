d76d06af5cbce859ba715feb7488491e74991132
Steve Jenkins <steve@skysteve.com>
2016-10-11 13:38:24 +0100

    remove allow js

---
+++ src/index.js
@@ -0,0 +1,4 @@
+"use strict";
+var ts_bug_example_1_1 = require('ts-bug-example-1');
+var result = ts_bug_example_1_1.sum(2, 3);
+console.log("2 + 3 = " + result);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {sum} from 'ts-bug-example-1';
 
-const result = sum(2, 3)
+const result = sum(2, 3);
 
 console.log(`2 + 3 = ${result}`);
